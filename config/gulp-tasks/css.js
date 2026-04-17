4d3705d811907e00b2171421c16a4760b29dbccd
Raman Martynau <rmartynau@gmail.com>
2026-04-17 20:32:19 +0300

    added changes in setting.json; fixed npm run build

--- config/gulp-tasks/css.js
+++ config/gulp-tasks/css.js
@@ -45,7 +45,7 @@ export const css = () => {
 		.pipe(
 			app.plugins.if(
 				app.isBuild,
-				replace(/(@import\s+url\(['"]?.*?;)\s+/g, '$1') // Видаляє пробіли
+				app.plugins.replace(/(@import\s+url\(['"]?.*?;)\s+/g, '$1') // Видаляє пробіли
 			)
 		)
 		.pipe(
